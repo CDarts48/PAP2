225ca675d9cad338798fb5592048d5e1a3a589f8
CDarts48 <cdartswebdev@gmail.com>
2024-10-20 16:46:11 -0600

    create react app

--- client/src/components/Property.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Property = ({ property }) => {
-  return <li>{property.name}</li>; // Replace 'name' with your actual property key
-};
-
-export default Property;
