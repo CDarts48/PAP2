46cd797579565af7b888161b089519cf356f3ab3
CDarts48 <cdartswebdev@gmail.com>
2024-10-20 18:49:55 -0600

    Looks pretty!

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import tebo from './assets/Tebo.png';
 import './App.css';
 import Properties from './components/Properties'; // Import the Properties component
@@ -10,16 +10,10 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={tebo} className="App-logo" alt="Tebo Logo" />
-          <div className="button-group">
-            <Link to="/properties">
-              <button>Properties</button>
-            </Link>
-            <button>Unit</button>
-            <button>Tenants</button>
-            <button>Payments</button>
-          </div>
+          <Content />
         </header>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/properties" element={<Properties />} />
           {/* Add other routes here */}
         </Routes>
@@ -27,5 +21,28 @@ function App() {
     </Router>
   );
 }
+
+const Content = () => {
+  const location = useLocation();
+  if (location.pathname === '/properties') {
+    return null; // Hide everything except the image
+  }
+  return (
+    <div className="button-group">
+      <Link to="/properties">
+        <button>Properties</button>
+      </Link>
+      <button>Unit</button>
+      <button>Tenants</button>
+      <button>Payments</button>
+    </div>
+  );
+};
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Home Page</h1>
+  </div>
+);
 
 export default App;
