404d1b43b7ec2a2d4796dd29af770345a3e0efa5
CDarts48 <cdartswebdev@gmail.com>
2024-12-03 20:54:07 -0700

    real og

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3001;
 
 // Use the CORS middleware
 app.use(cors({
-  origin: 'https://your-frontend-url.onrender.com', // Replace with your frontend URL
+  origin: 'https://pap2.onrender.com', 
   credentials: true
 }));
 
